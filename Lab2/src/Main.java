79224d470922f0c56aaa4a87762577a1412c674e
917_Turcu_Remus <78847479+Gabarsolon@users.noreply.github.com>
2023-10-30 20:13:11 +0200

    LAB-3: Started the implementation of the scan method

--- Lab2/src/Main.java
+++ Lab2/src/Main.java
@@ -2,27 +2,7 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        SymbolTable<String>symbolTable = new SymbolTable<>();
-        System.out.println(symbolTable.isEmpty());
-        symbolTable.add("a");
-        symbolTable.add("b");
-        symbolTable.add("c");
-        symbolTable.add("d");
-        symbolTable.add("3");
-        symbolTable.add("\"Message\"");
-        System.out.println(symbolTable.size());
-        System.out.println(symbolTable.get("b"));
-        System.out.println(symbolTable.remove("b"));
-        System.out.println(symbolTable.remove("b"));
-        System.out.println(symbolTable.get("b"));
-        System.out.println(symbolTable.get("3"));
-        System.out.println(symbolTable.add("a"));
-        System.out.println(symbolTable.get("a"));
-        PIF pif = new PIF();
-        System.out.println(pif);
-        pif.addIdentifierOrConstant("a", symbolTable.add("a"));
-        pif.addOperatorSeparatorReservedWord("while");
-        System.out.println(pif);
         var scanner = new Scanner("src/programs/p1.txt");
+        scanner.scan();
     }
 }
--- Lab2/src/Scanner.java
+++ Lab2/src/Scanner.java
@@ -1,13 +1,15 @@
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.StringTokenizer;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 
 public class Scanner {
+    String COMMENT_CHARACTER = "#";
     List<String> separators = Arrays.asList(
             "(", ")",
             "[", "]",
@@ -15,7 +17,7 @@ public class Scanner {
             ";", " ", ",", "\n", "\'", "\"", "#"
             );
     List<String> operators = Arrays.asList("+", "-", "*", "/", "%", "=", "<", "<=", "==>", ">", "===", "+=", "-=");
-    List<String> keyWords = Arrays.asList(
+    List<String> reservedWord = Arrays.asList(
             "int", "alpha", "arrr", "fibre", "make", "if", "fi", "of", "prgr",
             "read", "show", "define", "now", "persistent", "for", "while", "and",
             "or", "not", "starts", "from", "transforms", "stops", "at", "stdin", "stdout");
@@ -34,7 +36,35 @@ public class Scanner {
         programLines = bufferedReader.lines().toList();
     }
 
-    private void readFile() throws FileNotFoundException {
+    private boolean isIdentifier(String token){
+        return Pattern.compile("^[_a-zA-Z][_a-zA-Z0-9]*$").matcher(token).matches();
+    }
 
+    private void skipComment(StringTokenizer tokenizer){
+        while(tokenizer.hasMoreTokens())
+            //stop when the current token is equal to the comment char
+            if(tokenizer.nextToken().equals(COMMENT_CHARACTER))
+                return;
+    }
+
+    public void scan(){
+        String delimiters = separators.stream().reduce("", (a,b) -> a+b);
+
+        int index = 0;
+        for(var programLine : programLines){
+            StringTokenizer tokenizer = new StringTokenizer(programLine, delimiters, true);
+            while(tokenizer.hasMoreTokens()){
+                String currentToken = tokenizer.nextToken();
+                if(currentToken.equals(" "))
+                    continue;
+                if(currentToken.equals(COMMENT_CHARACTER))
+                    skipComment(tokenizer);
+                if(reservedWord.contains(currentToken) || operators.contains(currentToken) || separators.contains(currentToken))
+                    pif.addOperatorSeparatorReservedWord(currentToken);
+                else if(isIdentifier(currentToken))
+                    pif.addIdentifierOrConstant(currentToken, symbolTable.add(currentToken));
+            }
+        }
+        System.out.println(pif);
     }
 }
