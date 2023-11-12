edb2094e1b1803564056c610e0d974cbf83703e3
917_Turcu_Remus <78847479+Gabarsolon@users.noreply.github.com>
2023-11-12 13:22:45 +0200

    LAB-4: Create the FA class, added the constructor which reads the FA.in file and addded toString functions

---
+++ Lab2/src/FA.java
@@ -0,0 +1,93 @@
+import org.javatuples.Pair;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
+
+public class FA {
+    Set<String> states;
+    Set<String> alphabet;
+    Map<Pair<String, String>, String> transitions;
+    String initialState;
+    Set<String> finalStates;
+
+    public FA(String faFilePath) throws IOException {
+        var bufferedReader = new BufferedReader(new FileReader(faFilePath));
+
+        String statesLine = bufferedReader.readLine().replace("\n", "");
+        states = Set.of(statesLine.split(","));
+
+        String alphabetLine = bufferedReader.readLine().replace("\n", "");
+        alphabet = Set.of(alphabetLine.split(","));
+
+        transitions = new HashMap<>();
+        String transition = bufferedReader.readLine().replace("\n", "");
+        while (!transition.equals("transition_set_stop")) {
+            String[] transitionElements = transition.split(",");
+            String currentState = transitionElements[0];
+            String alphabetElement = transitionElements[1];
+            String nextState = transitionElements[2];
+            transitions.put(new Pair<>(currentState, alphabetElement), nextState);
+            transition = bufferedReader.readLine().replace("\n", "");
+        }
+
+        initialState = bufferedReader.readLine().replace("\n", "");
+
+        String finalStatesLine = bufferedReader.readLine().replace("\n", "");
+        finalStates = Set.of(finalStatesLine.split(","));
+
+        bufferedReader.close();
+    }
+
+    public String statesToString() {
+        var stringBuilder = new StringBuilder();
+        stringBuilder.append("Q = ");
+        stringBuilder.append(states.toString().replace("[", "{").replace("]", "}"));
+        return stringBuilder.toString();
+    }
+
+    public String alphabetToString(){
+        var stringBuilder = new StringBuilder();
+        stringBuilder.append("Σ = ");
+        stringBuilder.append(alphabet.toString().replace("[", "{").replace("]", "}"));
+        return stringBuilder.toString();
+    }
+
+    public String transitionsToString(){
+        var stringBuilder = new StringBuilder();
+        transitions.forEach((state_alphabet_pair, nextState) -> {
+            String currentState = state_alphabet_pair.getValue0();
+            String alphabetElement = state_alphabet_pair.getValue1();
+            stringBuilder.append("δ(%s,%s) = %s\n".formatted(currentState, alphabetElement, nextState));
+        });
+
+        //delete the last new line
+        stringBuilder.delete(stringBuilder.length() - 1, stringBuilder.length());
+
+        return stringBuilder.toString();
+    }
+
+    public String initialStateToString(){
+        return "q0 = %s".formatted(initialState);
+    }
+
+    public String finalStatesToString(){
+        var stringBuilder = new StringBuilder();
+        stringBuilder.append("F = ");
+        stringBuilder.append(finalStates.toString().replace("[", "{").replace("]", "}"));
+        return stringBuilder.toString();
+    }
+
+    @Override
+    public String toString() {
+        return "FA{" +
+                "states=" + states +
+                ", alphabet=" + alphabet +
+                ", transitions=" + transitions +
+                ", initialState='" + initialState + '\'' +
+                ", finalStates=" + finalStates +
+                '}';
+    }
+}
--- Lab2/src/Main.java
+++ Lab2/src/Main.java
@@ -2,7 +2,13 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        var scanner = new Scanner("src/programs/p2.txt");
-        scanner.scan();
+//        var scanner = new Scanner("src/programs/p2.txt");
+//        scanner.scan();
+        var fa = new FA("src/specifications/FA.in");
+        System.out.println(fa.statesToString());
+        System.out.println(fa.alphabetToString());
+        System.out.println(fa.transitionsToString());
+        System.out.println(fa.initialStateToString());
+        System.out.println(fa.finalStatesToString());
     }
 }
